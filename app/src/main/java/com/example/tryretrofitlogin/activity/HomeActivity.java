e31fee9fac6eb3c3ea0788b6f6df928fa39d32ef
Galang <galang221b@gmail.com>
2020-04-17 14:27:14 +0800

    init

--- app/src/main/java/com/example/tryretrofitlogin/activity/HomeActivity.java
+++ app/src/main/java/com/example/tryretrofitlogin/activity/HomeActivity.java
@@ -2,15 +2,40 @@ package com.example.tryretrofitlogin.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.tryretrofitlogin.R;
+import com.example.tryretrofitlogin.helper.SharedPrefManager;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private TextView txtusername, txtuseremail;
+    private Button btnlogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        txtusername = (TextView) findViewById(R.id.txt_username);
+        txtuseremail = (TextView) findViewById(R.id.txt_useremail);
+        btnlogout = (Button) findViewById(R.id.btn_logout);
+
+        btnlogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userLogout();
+            }
+        });
+    }
+
+    private void userLogout(){
+        SharedPrefManager.getInstance(this).logout();
+        finish();
+        startActivity(new Intent(this, MainActivity.class));
     }
 }
